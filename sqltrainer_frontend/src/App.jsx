3b6ab4cfedf9c9ced14deab5450c48c9d8f15217
Denis Markov <da.marckov2015@yandex.ru>
2026-04-26 22:26:04 +0700

    add editor in front

--- sqltrainer_frontend/src/App.jsx
+++ sqltrainer_frontend/src/App.jsx
@@ -1,11 +1,15 @@
-import { useState } from "react";
-import "./App.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import Editor from "@monaco-editor/react";
+import { LanguageIdEnum } from "monaco-sql-languages";
 
 function App() {
   return (
-    <>
-    </>
+    <Editor
+      height="90vh"
+      defaultLanguage={LanguageIdEnum.MYSQL}
+      defaultValue="-- SELECT * FROM table1;"
+    />
   );
 }
-
 export default App;
